783ad653b2e9b2b287a36f657a0aeff1e471c4d3
mikolaj_bochenek <mikolajbochenek.dev@gmail.com>
2020-02-10 19:11:44 +0100

    Db context creation

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeMugTask.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent API
+        }
+    }
+}
