98a2bbc48755206207304cf11a2d195efed3245d
mikolaj_bochenek <mikolajbochenek.dev@gmail.com>
2020-02-12 00:44:53 +0100

    pagination - create extension class for HttpResponse

---
+++ Helpers/Extensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+
+namespace CoffeeMugTask.API.Helpers
+{
+    public static class Extensions
+    {
+        public static void AddPagination(this HttpResponse response, int currentPage, int pageSize, int totalCount, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, pageSize, totalCount, totalPages);
+
+            var camelCaseFormatter = new JsonSerializerSettings();
+            camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
