a06bab3ca89437779a3d1dd6b5011d4bf5135360
mikolaj_bochenek <mikolajbochenek.dev@gmail.com>
2020-02-12 19:30:04 +0100

    Error renaming

--- DTOs/ProductForCreateDTO.cs
+++ DTOs/ProductForCreateDTO.cs
@@ -4,10 +4,10 @@ namespace CoffeeMugTask.API.DTOs
 {
     public class ProductForCreateDTO
     {
-        [Required(ErrorMessage = "Warrning: Name cannot be empty!")]
-        [MaxLength(100, ErrorMessage = "Warrining: Name cannot contains more than 100 characters")]
+        [Required(ErrorMessage = "BadRequest: Name cannot be empty!")]
+        [MaxLength(100, ErrorMessage = "BadRequest: Name cannot contains more than 100 characters")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Warrning: Price cannot be empty!")]
+        [Required(ErrorMessage = "BadRequest: Price cannot be empty!")]
         public decimal Price { get; set; }
     }
 }
--- DTOs/ProductFroUpdateDTO.cs
+++ DTOs/ProductFroUpdateDTO.cs
@@ -4,12 +4,12 @@ namespace CoffeeMugTask.API.DTOs
 {
     public class ProductFroUpdateDTO
     {
-        [Required(ErrorMessage = "Warrning: Id cannot be empty!")]
+        [Required(ErrorMessage = "BadRequest: Id cannot be empty!")]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Warrning: Name cannot be empty!")]
-        [MaxLength(100, ErrorMessage = "Warrining: Name cannot contains more than 100 characters")]
+        [Required(ErrorMessage = "BadRequest: Name cannot be empty!")]
+        [MaxLength(100, ErrorMessage = "BadRequest: Name cannot contains more than 100 characters")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Warrning: Price cannot be empty!")]
+        [Required(ErrorMessage = "BadRequest: Price cannot be empty!")]
         public decimal Price { get; set; }
     }
 }
